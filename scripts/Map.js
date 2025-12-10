44917f14439afcd0c628403af4e9bd9c757581b8
Bisferoz45 <gabriel.cardenas.martin@gmail.com>
2025-12-10 17:24:44 +0000

    Hecho el sistema de generación de zonas.
    Falta optimizarlo de manera en que no tome por válidas casillas ocupadas y genere todas las zonas.

--- scripts/Map.js
+++ scripts/Map.js
@@ -1,4 +1,4 @@
-export { generateMapBoard, getRandomCords }
+export { generateMapBoard, getRandomCords, setZoneOnMapBoard }
 
 function generateMapBoard(mapSize){
     let mapBoard = []
@@ -17,9 +17,116 @@ function getRandomCords(mapSize){
     return [row, col];
 }
 
-function setZoneOnMapBoard(mapBoard, boxCant, type){
+function setZoneOnMapBoard(mapBoard, cords, boxCant, type){
+    let ocupedBoxes = [`${cords[0]}-${cords[1]}`] //FILA - COLUMNA
+    mapBoard[cords[0]][cords[1]] = type;
+    boxCant--;
+    while(boxCant > 0){
+        let unocupedBoxes = getFreeBoxesAround(mapBoard, ocupedBoxes);
+        if(unocupedBoxes.length > 0){
+            let randomIndex = Math.floor(Math.random() * ((unocupedBoxes.length - 1) - 0) + 0);
+            let boxCords = unocupedBoxes[randomIndex].split("-")
+            let r = boxCords[0];
+            let c = boxCords[1];
+            mapBoard[r][c] = type;
+            ocupedBoxes.push(`${r}-${c}`);
+            boxCant--;
+        }else{
+            boxCant = 0;
+        }
+    }
+}
+
+function getFreeBoxesAround(mapBoard, ocupedBoxes){
+    let array = []
+    ocupedBoxes.forEach((element) => {
+        let cords = element.split("-");
+        let r = Number(cords[0]);
+        let c = Number(cords[1]);
+
+        if(r < mapBoard.length-1 && c < mapBoard[0].length-1 && r > 0 && c > 0){
+            array.push(getCordsOfDirecction(mapBoard, [r, c], "RIGHT"));
+            array.push(getCordsOfDirecction(mapBoard, [r, c], "RIGHT-DOWN"));
+            array.push(getCordsOfDirecction(mapBoard, [r, c], "DOWN"));
+            array.push(getCordsOfDirecction(mapBoard, [r, c], "LEFT-DOWN"));
+            array.push(getCordsOfDirecction(mapBoard, [r, c], "LEFT"));
+            array.push(getCordsOfDirecction(mapBoard, [r, c], "LEFT-UP"));
+            array.push(getCordsOfDirecction(mapBoard, [r, c], "UP"));
+            array.push(getCordsOfDirecction(mapBoard, [r, c], "RIGHT-UP"));
+        }else{
+            if(r >= mapBoard.length-1){
+                if(c >= mapBoard[0].length-1){
+                    array.push(getCordsOfDirecction(mapBoard, [r, c], "UP"));
+                    array.push(getCordsOfDirecction(mapBoard, [r, c], "LEFT-UP"));
+                    array.push(getCordsOfDirecction(mapBoard, [r, c], "LEFT"));
+                }else{
+                    array.push(getCordsOfDirecction(mapBoard, [r, c], "UP"));
+                    array.push(getCordsOfDirecction(mapBoard, [r, c], "RIGHT-UP"));
+                    array.push(getCordsOfDirecction(mapBoard, [r, c], "RIGHT"));
+                }
+            }else{
+                if(c >= mapBoard[0].length-1){
+                    array.push(getCordsOfDirecction(mapBoard, [r, c], "DOWN"));
+                    array.push(getCordsOfDirecction(mapBoard, [r, c], "LEFT-DOWN"));
+                    array.push(getCordsOfDirecction(mapBoard, [r, c], "LEFT"));
+                }else{
+                    array.push(getCordsOfDirecction(mapBoard, [r, c], "DOWN"));
+                    array.push(getCordsOfDirecction(mapBoard, [r, c], "RIGHT-DOWN"));
+                    array.push(getCordsOfDirecction(mapBoard, [r, c], "RIGHT"));
+                }
+            }
+        }
+    });
+
+    return array;
+}
+
+function getCordsOfDirecction(mapBoard, cords, direction){
+    switch(direction){
+        case "RIGHT":
+            if(mapBoard[cords[0]][cords[1] + 1] == 0){
+                return `${cords[0]}-${cords[1] + 1}`;
+            }else{
+                return 0;
+            }
+        break;
+        
+        case "RIGHT-DOWN":
+            if(mapBoard[cords[0] + 1][cords[1] + 1]){
+                return `${cords[0] + 1}-${cords[1] + 1}`;
+            }
+        break;
+        
+        case "DOWN":
+            return `${cords[0] + 1}-${cords[1]}`;
+        break;
+        
+        case "LEFT-DOWN":
+            return `${cords[0] + 1}-${cords[1] - 1}`;
+        break;
+        
+        case "LEFT":
+            return `${cords[0]}-${cords[1] - 1}`;
+        break;
+        
+        case "LEFT-UP":
+            return `${cords[0] - 1}-${cords[1] - 1}`;
+        break;
+        
+        case "UP":
+            return `${cords[0] - 1}-${cords[1]}`;
+        break;
+        
+        case "RIGHT-UP":
+            return `${cords[0] - 1}-${cords[1] + 1}`;
+        break;
+        default:
+            return "INCORRECT VALUE OF 'DIRECTION'"
+        break;
+    }
     
 }
+
 
 function getDirectionExpand(){
     switch(Math.floor(Math.random() * (8 - 1) + 1)){
--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,5 @@
 import { tags } from "../resources/TagReferences.js";
-import { generateMapBoard, getRandomCords } from "./map.js";
+import { generateMapBoard, getRandomCords, setZoneOnMapBoard } from "./map.js";
 
 document.addEventListener("DOMContentLoaded", (event) => {
     document.getElementById("map-form").addEventListener("submit", (event) => {
@@ -9,27 +9,47 @@ document.addEventListener("DOMContentLoaded", (event) => {
                                                                                //↓ CANTIDAD DE ZONAS DE LAS QUE DISPONDRÁ ↓\\
         const natureData = [Math.floor(Math.random() * (document.querySelector("input[name=N-MnZ]").value - document.querySelector("input[name=N-MxZ]").value) + document.querySelector("input[name=N-MxZ]").value),
                             document.querySelector("input[name=N-ZMS]").value, //NATURE - ZONE MAX SIZE
-                            document.querySelector("input[name=N-TMS]").value] //NATURE - TOTAL MAX SIZE
+                            document.querySelector("input[name=N-TMS]").value] //NATURE - TOTAL MAX BOXES
 
                                                                               //↓ CANTIDAD DE ZONAS DE LAS QUE DISPONDRÁ ↓\\
         const urbanData = [Math.floor(Math.random() * (document.querySelector("input[name=U-MnZ]").value - document.querySelector("input[name=U-MxZ]").value) + document.querySelector("input[name=U-MxZ]").value),
                            document.querySelector("input[name=U-ZMS]").value, //URBAN - ZONE MAX SIZE
-                           document.querySelector("input[name=U-TMS]").value] //URBAN - TOTAL MAX SIZE
+                           document.querySelector("input[name=U-TMS]").value] //URBAN - TOTAL MAX BOXES
 
                                                                                    //↓ CANTIDAD DE ZONAS DE LAS QUE DISPONDRÁ ↓\\
         const commertialData = [Math.floor(Math.random() * (document.querySelector("input[name=U-MnZ]").value - document.querySelector("input[name=U-MxZ]").value) + document.querySelector("input[name=U-MxZ]").value),
                                 document.querySelector("input[name=U-ZMS]").value, //COMMERTIAL - ZONE MAX SIZE
-                                document.querySelector("input[name=U-TMS]").value] //COMMERTIAL - TOTAL MAX SIZE
+                                document.querySelector("input[name=U-TMS]").value] //COMMERTIAL - TOTAL MAX BOXES
         
         let mapBoard = generateMapBoard(mapSize);
         let cords = getRandomCords(mapSize);
-        mapBoard[cords[0]][cords[1]] = getRandomType();
-        console.log(mapBoard);
+        let type = getRandomType();
         
+        switch(type){
+            case 1:
+                setZoneOnMapBoard(mapBoard, cords, getZoneSize(natureData), type);
+            break;
+            
+            case 2:
+                setZoneOnMapBoard(mapBoard, cords, getZoneSize(urbanData), type);
+            break;
+            
+            case 3:
+                setZoneOnMapBoard(mapBoard, cords, getZoneSize(commertialData), type);
+            break;
+            
+            default:
+                console.log(`ERROR: El tipo '${type} no es un valor aceptado'`);
+            break;
+        }
+        
+        console.log(mapBoard);
     });
 });
 
-function getZoneSize(){} //Devuelve la cantidad de casillas con las que contara una zona
+function getZoneSize(data){ //Devuelve la cantidad de casillas con las que contara una zona
+    return Math.floor(Math.random() * (data[1] - 1) + 1);
+}
 
 function getRandomType(){
     return Math.floor(Math.random() * (3 - 1) + 1);
